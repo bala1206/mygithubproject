7238922a2e828560b5b991c433e2fc372e4debce
bala1206 <Personal@Personal-PC>
2022-05-09 17:04:41 +0530

    first commit - my first github project

--- mygithubproject/src/demos/Myfirstgithubproject.java
+++ mygithubproject/src/demos/Myfirstgithubproject.java
@@ -4,7 +4,7 @@ public class Myfirstgithubproject {
 
 	public static void main(String[] args) {
 
-		System.out.println("This is my first Test Case 1 for git hub");
+		System.out.println("This was my first Test Case 1 for git hub");
 	}
 
 }
