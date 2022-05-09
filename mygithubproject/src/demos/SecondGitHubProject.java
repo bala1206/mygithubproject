a2a06c6be1aa0cc3f7a086f4364b7b2f50dcc8d4
bala1206 <Personal@Personal-PC>
2022-05-09 17:16:35 +0530

    Second git hub project

---
+++ mygithubproject/src/demos/SecondGitHubProject.java
@@ -0,0 +1,11 @@
+package demos;
+
+public class SecondGitHubProject {
+
+	public static void main(String[] args) {
+		
+		System.out.println("This was my second Test Case 2 for git hub");
+
+	}
+
+}
